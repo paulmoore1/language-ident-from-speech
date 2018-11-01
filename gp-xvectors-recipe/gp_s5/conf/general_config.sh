7552d160c2843565fc8c4d6365736a4d532a6d78
samsucik <sucik.samo@gmail.com>
2018-11-01 12:39:30 +0000

    Fixed calculation of max. number of jobs.

--- gp-xvectors-recipe/gp_s5/conf/general_config.sh
+++ gp-xvectors-recipe/gp_s5/conf/general_config.sh
@@ -3,4 +3,7 @@
 # This config contains options that can be set automatically depending on the
 # identified context (i.e. machine, for example Sam's DICE or a cluster node).
 
-MAXNUMJOBS=$(grep -c ^processor /proc/cpuinfo)
+num_threads_per_core=$(lscpu | grep -oP '^Thread.*\K([0-9]+)')
+num_cores_per_CPU=$(lscpu | grep -oP '^Core.*\K([0-9]+)')
+num_CPUs=$(lscpu | grep -oP '^CPU\(.*\K([0-9]+)')
+MAXNUMJOBS=$(($num_CPUs*$num_cores_per_CPU*$num_threads_per_core))
