0a436906ca216c1160f497ebeecdf45d163b8ebf
samsucik <sucik.samo@gmail.com>
2018-12-04 15:16:17 +0000

    Added Conda installation script.

--- install_conda.sh
+++ install_conda.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+source ~/.bashrc
+
 if [ -x "$(command -v conda)" ]; then
   echo 'Conda installed and in PATH. Skipping installation.' >&2
-  exit
 else
   echo "Conda not installed. Installing it."
+  mkdir $HOME/miniconda
+  pushd $HOME/miniconda
+
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+  bash ./miniconda.sh -b -p $HOME/miniconda -f
+  echo 'export PATH="$PATH:$HOME/miniconda/bin"' >> ~/.bashrc
+  source ~/.bashrc
+  popd
 fi
 
-mkdir $HOME/miniconda
-cd $HOME/miniconda
+if [ ! "$(conda env list | grep lid)" ]; then 
+  echo "Conda environment does not exist. Creating it."
+  conda env create -f ./environment.yml
+fi
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
-bash ./miniconda.sh -b -p $HOME/miniconda -f
-echo 'export PATH="$PATH:$HOME/miniconda/bin"' >> ~/.bashrc
-source ~/.bashrc
+source activate lid
