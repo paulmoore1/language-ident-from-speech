f31f830494fd355140da59b4a0fb9f0c8d535d9b
Sam Sucik <sucik.samo@gmail.com>
2018-12-16 22:54:34 +0000

    Removed trash from cmd.sh.

--- gp-xvectors-recipe/cmd.sh
+++ gp-xvectors-recipe/cmd.sh
@@ -1,23 +1,6 @@
 # Forked from egs/gp/s5/cmd.sh
 # Author: Sam Sucik
 
-# "queue.pl" uses qsub.  The options to it are
-# options to qsub.  If you have GridEngine installed,
-# change this to a queue you have access to.
-# Otherwise, use "run.pl", which will run jobs locally
-# (make sure your --num-jobs options are no more than
-# the number of cpus on your machine.
-
-# At BUT use:
-# export train_cmd="queue.pl -q all.q@@stable -l ram_free=1G,mem_free=1G"
-# export decode_cmd="queue.pl -q all.q@@stable -l ram_free=2G,mem_free=2G"
-# export highmem_cmd="queue.pl -q all.q@@stable -l ram_free=2G,mem_free=2G"
-
-# On Eddie use:
-# export train_cmd="queue.pl -P inf_hcrc_cstr_general"
-# export decode_cmd="queue.pl -P inf_hcrc_cstr_general"
-# export highmem_cmd="queue.pl -P inf_hcrc_cstr_general -pe memory-2G 2"
-
 source ./helper_functions.sh
 
 # To run locally, use:
@@ -27,21 +10,12 @@ if [[ $(whichMachine) = "sam" ]] || [[ $(whichMachine) = "dice"* ]]; then
 	export train_cmd=run.pl
 	export extract_cmd=run.pl
 	export plda_cmd=run.pl
-
-	export decode_cmd=run.pl
-	export highmem_cmd=run.pl
 elif [[ $(whichMachine) == cluster* ]]; then
 	echo "Running on the cluster."
-	# export train_cmd=slurm.pl
-	# export decode_cmd=slurm.pl
-	# export highmem_cmd=slurm.pl
 	export preprocess_cmd=run.pl
 	export train_cmd=slurm.pl
 	export extract_cmd=slurm.pl
 	export plda_cmd=run.pl
-
-	export decode_cmd=run.pl
-	export highmem_cmd=run.pl
 else
 	echo "Running on an unrecognised machine."
 fi
