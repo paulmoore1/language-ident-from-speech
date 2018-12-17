3894a9c8d06d9fba5e6dc93a0eee2a4ba37b9a38
Sam Sucik <sucik.samo@gmail.com>
2018-12-17 09:57:37 +0000

    Changed classification stats computing.

--- gp-xvectors-recipe/local/classify_scores.py
+++ gp-xvectors-recipe/local/classify_scores.py
@@ -4,8 +4,10 @@ Takes the scores file as input, prints the error rate for each language.
 For each utterance, classifies it as the most likely of all the languages scores
 """
 from __future__ import print_function
-import re, os, argparse, sys, math, warnings, random
-
+import re, os, argparse, sys, math, warnings, random, pprint
+import numpy as np
+from collections import OrderedDict
+from sklearn.metrics import confusion_matrix
 
 
 def get_args():
@@ -16,83 +18,53 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def parse_file(score_filename):
-    with open(score_filename, "r") as f:
-        all_lines = f.readlines()
-    # Assuming scores are in the format:
-    # X utt_1 0.2
-    # Y utt_1 0.4
-    # Z utt_1 0.6
-    # X utt_2 0.3
-    # ...and so on
-    languages = []
-    for line in all_lines:
-        entry = line.split(" ")
-        new_lang = entry[0]
-        if new_lang not in languages:
-            languages.append(new_lang)
-        else:
-            break
-    return languages, all_lines
+def classify(scores_file, output_file):
+    with open(scores_file, "r") as f:
+        lines = f.readlines()
+        lines = np.array([line.split() for line in lines])
+        languages = OrderedDict((x, True) for x in lines[:, 0]).keys()
+        utterances = OrderedDict((x, True) for x in lines[:, 1]).keys()
+        grouped_lines = OrderedDict((utt, np.array([line for line in lines if line[1] == utt])) for utt in utterances)
 
-def check_classification(language, data_portion):
-    target_lang = data_portion[0][3:5]
-    pred_lang = ""
-    max_score = -9999
-    for line in data_portion:
-        entry = line.split(" ")
-        score = float(entry[2])
-        if score > max_score:
-            max_score = score
-            pred_lang = entry[0]
-    if target_lang == pred_lang:
-        return True
-    else:
-        return False
+        with open(output_file, "w") as o:
+            for utt, lines in grouped_lines.items():
+                true_lang = utt[:2]
+                scores = np.array(lines[:, 2])
+                predicted_lang = lines[np.argmax(scores)][0]
+                o.write("{} {}\n".format(true_lang, predicted_lang))
 
+    return list(languages)
 
-def get_language_error(data, language, step_size):
-    num_correct = 0
-    num_wrong = 0
-    for idx, line in enumerate(data):
-        if idx % step_size != 0:
-            continue
-        else:
-            entry = line.split(" ")
-            utt_lang = entry[1][0:2]
-            if utt_lang == language:
-                correct = check_classification(language, data[idx:idx+step_size])
-            else:
-                continue
-        if correct:
-            num_correct += 1
-        else:
-            num_wrong += 1
-    total = num_correct + num_wrong
-    if total == 0:
-        return 100
-    else:
-        return float(num_wrong)/total*100
+def make_stats(classification_file, output_file, languages):
+    with open(classification_file, "r") as f:
+        classifications = np.array([line.split() for line in f.readlines()])
+        y_true = classifications[:, 0]
+        y_pred = classifications[:, 1]
+        conf_matrix = confusion_matrix(y_true, y_pred, labels=languages)
 
+        accuracy = np.trace(conf_matrix)/np.sum(conf_matrix)
+        acc_msg = "Accuracy: {:.3f} ({}/{} classified correctly)"\
+            .format(accuracy, np.trace(conf_matrix), np.sum(conf_matrix))
+        print(acc_msg)
+        
+        conf_matrix_nice = [" ".join([str(e) for e in row]) for row in conf_matrix]
+        conf_mtrx_msg = "Confusion matrix:\n{}\n{}".format("  ".join(languages), "\n".join(conf_matrix_nice))
+        print(conf_mtrx_msg)
+
+        with open(output_file, "w") as o:
+            o.write("{}\n".format(acc_msg))
+            o.write("{}\n".format(conf_mtrx_msg))
 
 def main():
     args = get_args()
-    os.chdir(args.scores_dir)
-    languages, data = parse_file("lang_eval_scores")
-    step_size = len(languages)
-    language_errors = []
-    for language in languages:
-        language_error = get_language_error(data, language, step_size)
-        str_out = "Error for language {} is {:.1f}%".format(language, language_error)
-        print(str_out)
-        language_errors.append(str_out)
+
+    scores_file = os.path.join(args.scores_dir, "lang_eval_scores")
+    classification_file = os.path.join(args.scores_dir, "classification_results")
+    stats_file = os.path.join(args.scores_dir, "stats")
     
-    with open("lang_classification_errors", "w") as f:
-        for error in language_errors:
-            f.write("{}\n".format(error))
+    languages = classify(scores_file, classification_file)
 
-
-
+    make_stats(classification_file, stats_file, languages)
 
 
 if __name__ == "__main__":
