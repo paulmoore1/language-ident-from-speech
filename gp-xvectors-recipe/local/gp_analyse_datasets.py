4641cd5f667d9e6c29a11352c3bf1360384e3e30
Sam Sucik <sucik.samo@gmail.com>
2019-01-14 23:33:26 +0000

    Created code for generating all 4 sets. Now gonna debug it in the cluster.

--- gp-xvectors-recipe/local/gp_analyse_datasets.py
+++ gp-xvectors-recipe/local/gp_analyse_datasets.py
@@ -95,18 +95,87 @@ if __name__ == "__main__":
         available_spks = [spk_id for spk_id in all_spk_lists[L] if \
                           (spk_id not in eval_set and spk_id not in test_set)]
         # print("Choosing from speakers: {}".format(available_spks))
+        
+        spks_test = []
+        spks_eval = []
+        spks_enroll = []
+        spks_train = []
 
         if (len(eval_set) + len(test_set)) == 0:
             print("No original split found for {}. Creating all four datasets.".format(L))
+            num_enroll = int(ceil(0.1*num_spks[L]))
+            num_test = num_enroll
+            num_eval = num_enroll
 
             if L in langs_with_spk_data:
                 print("Spk metadata found for {}. Using it for the split.".format(L))
                 # The most complicated case: 4 non-random sets
-                continue
+                sets_valid = False
+                attempt_counter = 0
+                threshold = 0
+                max_iter = 100
+                while not sets_valid:
+                    spks_test = random.sample(available_spks, num_test)
+                    spks_eval = random.sample([spk_id for spk_id in available_spks if spk_id not in spks_test], num_eval)
+                    spks_enroll = random.sample([spk_id for spk_id in available_spks if (spk_id not in spks_test) and (spk_id not in spks_eval)], num_enroll)
+                    spks_train = [spk_id for spk_id in available_spks if (spk_id not in spks_test) and (spk_id not in spks_eval) and (spk_id not in spks_enroll)]
+                    
+                    test_articles = []
+                    for spk_id in spks_test:
+                        test_articles = test_articles + spk_article_dict[L][spk_id]                    
+                    test_articles = set(test_articles)
+                    
+                    eval_articles = []
+                    for spk_id in spks_eval:
+                        eval_articles = eval_articles + spk_article_dict[L][spk_id]                    
+                    eval_articles = set(eval_articles)
+
+                    enroll_articles = []
+                    for spk_id in spks_enroll:
+                        enroll_articles = enroll_articles + spk_article_dict[L][spk_id]                    
+                    enroll_articles = set(enroll_articles)
+
+                    train_articles = []
+                    for spk_id in spks_train:
+                        train_articles = train_articles + spk_article_dict[L][spk_id]
+                    train_articles = set(train_articles)
+
+                    overlap = len(test_articles) + len(eval_articles) + len(enroll_articles) + len(train_articles) \
+                              - len(train_articles.intersection(test_articles, eval_articles, enroll_articles))
+
+                    if len(overlap) <= threshold:
+                        print("\nVALID SPLIT FOUND ({})\nTest: {}\nEval: {}\nEnroll: {}\nTrain: {}\nVALID SPLIT FOUND\n"\
+                              .format(attempt_counter, spks_test, spks_eval, spks_enroll, spks_train))
+                        break
+                    else:
+                        attempt_counter += 1
+                        if attempt_counter % max_iter == 0:
+                            threshold += 1
+                            print("Looking for splits with <= {} overlaps.".format(threshold))
+                        pass
+                        # print("Invalid split (overlap: {}).".format(len(overlap)))
             else:
                 print("No spk metadata found for {}. Doing random split.".format(L))
                 # The easy case: 4 random sets
-                continue
+                spks_test = random.sample(available_spks, num_test)
+                spks_eval = random.sample([spk_id for spk_id in available_spks if spk_id not in spks_test], num_eval)
+                spks_enroll = random.sample([spk_id for spk_id in available_spks if (spk_id not in spks_test) and (spk_id not in spks_eval)], num_enroll)
+                spks_train = [spk_id for spk_id in available_spks if (spk_id not in spks_test) and (spk_id not in spks_eval) and (spk_id not in spks_enroll)]
+                print("Testing speakers: {}".format(spks_test))
+                print("Evaluation speakers: {}".format(spks_eval))
+                print("Enrollment speakers: {}".format(spks_enroll))
+                print("Training speakers: {}".format(spks_train))
+                # continue
+            
+            with open("speakers/{}_test".format(L), "w") as f:
+                f.write(' '.join(spks_test))
+            with open("speakers/{}_eval".format(L), "w") as f:
+                f.write(' '.join(spks_eval))
+            with open("speakers/{}_enroll".format(L), "w") as f:
+                f.write(' '.join(spks_enroll))
+            with open("speakers/{}_train".format(L), "w") as f:
+                f.write(' '.join(spks_train))
+
         else:
             print("Original split found for {}. Creating only the train and enroll datasets.".format(L))
             num_enroll = int(ceil(0.1*num_spks[L]))
@@ -136,10 +205,6 @@ if __name__ == "__main__":
                     overlap = list(train_articles.intersection(enroll_articles))
                     if len(overlap) <= threshold:
                         print("\nVALID SPLIT FOUND ({})\nTrain: {}\nEnroll: {}\nVALID SPLIT FOUND\n".format(attempt_counter, spks_train, spks_enroll))
-                        with open("speakers/{}_enroll".format(L), "w") as f:
-                            f.write(' '.join(spks_enroll))
-                        with open("speakers/{}_train".format(L), "w") as f:
-                            f.write(' '.join(spks_train))
                         break
                     else:
                         attempt_counter += 1
@@ -156,3 +221,7 @@ if __name__ == "__main__":
                 print("Enrollment speakers: {}".format(spks_enroll))
                 print("Training speakers: {}".format(spks_train))
                 
+            with open("speakers/{}_enroll".format(L), "w") as f:
+                f.write(' '.join(spks_enroll))
+            with open("speakers/{}_train".format(L), "w") as f:
+                f.write(' '.join(spks_train))
