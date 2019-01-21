d289e9f25582506056ba903a48934f2f3869b16f
Sam Sucik <sucik.samo@gmail.com>
2019-01-21 23:53:20 +0000

    Adjusted the way the experiment config works, going to test its use in the cluster.

--- gp-xvectors-recipe/path.sh
+++ gp-xvectors-recipe/path.sh
@@ -25,9 +25,9 @@ export PATH=$PATH:$SCRIPTS
 
 # If the correct version of shorten and sox are not on the path,
 # the following will be set by local/gp_check_tools.sh
-SHORTEN_BIN=/afs/inf.ed.ac.uk/user/s15/s1531206/language-ident-from-speech/gp-xvectors-recipe/tools/shorten-3.6.1/bin
+SHORTEN_BIN=/home/samo/edi/hp/project/gp-xvectors-recipe/tools/shorten-3.6.1/bin
 # e.g. $PWD/tools/shorten-3.6.1/bin
-SOX_BIN=/afs/inf.ed.ac.uk/user/s15/s1531206/language-ident-from-speech/gp-xvectors-recipe/tools/sox-14.3.2/bin
+SOX_BIN=/home/samo/edi/hp/project/gp-xvectors-recipe/tools/sox-14.3.2/bin
 # e.g. $PWD/tools/sox-14.3.2/bin
 export PATH=$SHORTEN_BIN:$PATH
 export PATH=$SOX_BIN:$PATH
