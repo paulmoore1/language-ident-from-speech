a45e8b82323134d159d0c9f3d5b9211b0593abd2
Paul Moore <lapilosew2003@gmail.com>
2019-02-22 11:01:58 +0000

    Setup transfer of results

--- transfer_results.py
+++ transfer_results.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, shutil
 
 # Allow to run with Python 3 or 2.
 def listdir_no_arguments():
@@ -8,11 +8,21 @@ def listdir_no_arguments():
         return os.listdir('.')
 
 def main():
-    os.chdir("..")
-    os.chdir("gp-data")
+    copy_from_path=os.path.join("home", "s1531206", "gp-data")
+    assert os.path.exists(copy_from_path), "Path not found at {}".format(copy_from_path)
+    os.chdir(copy_from_path)
+
+    copy_to_path=os.path.join("afs", "inf.ed.ac.uk", "user", "s15", "s1531206", "lre_results")
+    if not os.path.exists(copy_to_path):
+        os.mkdir(copy_to_path)
+
     exp_dirs = listdir_no_arguments()
     for exp_dir in exp_dirs:
-        is os.path.exists()
+        results_path_from = os.path.join(exp_dir, "exp", "results", "results")
+        results_path_to = os.path.join(copy_to_path, exp_dir)
+        if os.path.exists(results_path_from):
+            os.mkdir(results_path_to)
+            shutil.copy(results_path_from, results_path_to)
 
 if __name__ == "__main__":
     main()
