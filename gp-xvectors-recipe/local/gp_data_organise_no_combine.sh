4d1b6c131198adbf9af34083229d401b8d161d8f
Paul Moore <lapilosew2003@gmail.com>
2019-02-23 13:03:33 +0000

    Setup one-time data prep

---
+++ gp-xvectors-recipe/local/gp_data_organise_no_combine.sh
@@ -0,0 +1,224 @@
+#!/bin/bash -u
+
+# Copyright 2012  Arnab Ghoshal
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+
+function error_exit () {
+  echo -e "$@" >&2; exit 1;
+}
+
+function read_dirname () {
+  local dir_name=`expr "X$1" : '[^=]*=\(.*\)'`;
+  [ -d "$dir_name" ] || mkdir -p "$dir_name" || error_exit "Directory '$dir_name' not found";
+  local retval=`cd $dir_name 2>/dev/null && pwd || exit 1`
+  echo $retval
+}
+
+PROG=`basename $0`;
+usage="Usage: $PROG <arguments>\n
+Prepare train, enroll, eval and test file lists for a language.\n
+e.g.: $PROG --config-dir=conf --corpus-dir=corpus --languages=\"GE PO SP\"\n\n
+Required arguments:\n
+  --config-dir=DIR\tDirecory containing the necessary config files\n
+  --corpus-dir=DIR\tDirectory for the GlobalPhone corpus\n
+  --train-languages=STR\tSpace separated list of two letter language codes for training\n
+  --enroll-languages=STR\tSpace separated list of two letter language codes for enrollment\n
+  --eval-languages=STR\tSpace separated list of two letter language codes for evaluation\n
+  --test-languages=STR\tSpace separated list of two letter language codes for testing\n
+";
+
+if [ $# -lt 4 ]; then
+  error_exit $usage;
+fi
+
+while [ $# -gt 0 ];
+do
+  case "$1" in
+  --help) echo -e $usage; exit 0 ;;
+  --config-dir=*)
+  CONFDIR=`read_dirname $1`; shift ;;
+  --corpus-dir=*)
+  GPDIR=`read_dirname $1`; shift ;;
+  --train-languages=*)
+  TRAIN_LANGUAGES=`expr "X$1" : '[^=]*=\(.*\)'`; shift ;;
+  --enroll-languages=*)
+  ENROLL_LANGUAGES=`expr "X$1" : '[^=]*=\(.*\)'`; shift ;;
+  --eval-languages=*)
+  EVAL_LANGUAGES=`expr "X$1" : '[^=]*=\(.*\)'`; shift ;;
+  --test-languages=*)
+  TEST_LANGUAGES=`expr "X$1" : '[^=]*=\(.*\)'`; shift ;;
+  --data-dir=*)
+  datadir=`read_dirname $1`; shift ;;
+  --wav-dir=*)
+  WAVDIR=`read_dirname $1`; shift ;;
+  *)  echo "Unknown argument: $1, exiting"; echo -e $usage; exit 1 ;;
+  esac
+done
+
+# Check if the config files are in place:
+pushd $CONFDIR > /dev/null
+if [ -f test_spk.list ]; then
+  test_list=$CONFDIR/test_spk.list
+else
+  echo "Test-set speaker list not found."; exit 1
+fi
+if [ -f eval_spk.list ]; then
+  eval_list=$CONFDIR/eval_spk.list
+else
+  echo "Eval-set speaker list not found."; exit 1
+fi
+if [ -f enroll_spk.list ]; then
+  enroll_list=$CONFDIR/enroll_spk.list
+else
+  echo "Enrollment-set speaker list not found."; exit 1
+fi
+if [ -f train_spk.list ]; then
+  train_list=$CONFDIR/train_spk.list
+fi
+popd > /dev/null
+
+[ -f path.sh ] && . ./path.sh  # Sets the PATH to contain necessary executables
+
+# Make data folders to contain all the language files.
+for x in train enroll eval test; do
+  mkdir -p $datadir/${x}
+done
+
+tmpdir=$(mktemp -d /tmp/kaldi.XXXX);
+trap 'rm -rf "$tmpdir"' EXIT
+
+# Create directories to contain files needed in training and testing:
+echo "datadir is: $datadir"
+for L in $TRAIN_LANGUAGES; do
+  (
+  mkdir -p $tmpdir/train/$L
+  if [ -f $CONFDIR/train_spk.list ]; then
+    grep "^$L" $train_list | cut -f2- | tr ' ' '\n' \
+      | sed -e "s?^?$L?" -e 's?$?_?' > $tmpdir/train/$L/train_spk
+  else
+    echo "Train-set speaker list not found. Skipping."
+    #grep -v -f $tmpdir/$L/test_spk -f $tmpdir/$L/eval_spk -f $tmpdir/$L/enroll_spk \
+    #  $WAVDIR/$L/lists/spk > $tmpdir/$L/train_spk || \
+    #  echo "Could not find any training set speakers; \
+    #  are you trying to use all of them for evaluation and testing?";
+    continue
+  fi
+
+  echo "Language - ${L}: formatting train data."
+  mkdir -p $datadir/$L/train
+  rm -f $datadir/$L/train/wav.scp $datadir/$L/train/spk2utt \
+        $datadir/$L/train/utt2spk $datadir/$L/train/utt2len
+
+  for spk in `cat $tmpdir/train/$L/train_spk`; do
+    grep -h "$spk" $WAVDIR/$L/lists/wav.scp >> $datadir/$L/train/wav.scp
+    grep -h "$spk" $WAVDIR/$L/lists/spk2utt >> $datadir/$L/train/spk2utt
+    grep -h "$spk" $WAVDIR/$L/lists/utt2spk >> $datadir/$L/train/utt2spk
+    grep -h "$spk" $WAVDIR/$L/lists/utt2len >> $datadir/$L/train/utt2len
+  done
+  ) &
+  wait;
+  sed -e 's?[0-9]*$??' $datadir/${L}/train/utt2spk \
+  > $datadir/${L}/train/utt2lang
+
+  ./local/utt2lang_to_lang2utt.pl $datadir/${L}/train/utt2lang \
+  > $datadir/${L}/train/lang2utt
+done
+wait;
+echo "Done"
+
+for L in $ENROLL_LANGUAGES; do
+  (
+  mkdir -p $tmpdir/enroll/$L
+  grep "^$L" $enroll_list | cut -f2- | tr ' ' '\n' \
+    | sed -e "s?^?$L?" -e 's?$?_?' > $tmpdir/enroll/$L/enroll_spk
+  echo "Language - ${L}: formatting enroll data."
+  mkdir -p $datadir/$L/enroll
+  rm -f $datadir/$L/enroll/wav.scp $datadir/$L/enroll/spk2utt \
+        $datadir/$L/enroll/utt2spk $datadir/$L/enroll/utt2len
+
+  for spk in `cat $tmpdir/enroll/$L/enroll_spk`; do
+    grep -h "$spk" $WAVDIR/$L/lists/wav.scp >> $datadir/$L/enroll/wav.scp
+    grep -h "$spk" $WAVDIR/$L/lists/spk2utt >> $datadir/$L/enroll/spk2utt
+    grep -h "$spk" $WAVDIR/$L/lists/utt2spk >> $datadir/$L/enroll/utt2spk
+    grep -h "$spk" $WAVDIR/$L/lists/utt2len >> $datadir/$L/enroll/utt2len
+  done
+  ) &
+  wait;
+  sed -e 's?[0-9]*$??' $datadir/${L}/enroll/utt2spk \
+  > $datadir/${L}/enrolltest/utt2lang
+
+  ./local/utt2lang_to_lang2utt.pl $datadir/${L}/enroll/utt2lang \
+  > $datadir/${L}/enroll/lang2utt
+done
+wait;
+echo "Done"
+
+for L in $EVAL_LANGUAGES; do
+  (
+  mkdir -p $tmpdir/eval/$L
+  grep "^$L" $eval_list | cut -f2- | tr ' ' '\n' \
+    | sed -e "s?^?$L?" -e 's?$?_?' > $tmpdir/eval/$L/eval_spk
+
+  echo "Language - ${L}: formatting eval data."
+  mkdir -p $datadir/$L/eval
+  rm -f $datadir/$L/eval/wav.scp $datadir/$L/eval/spk2utt \
+        $datadir/$L/eval/utt2spk $datadir/$L/eval/utt2len
+
+  for spk in `cat $tmpdir/eval/$L/eval_spk`; do
+    grep -h "$spk" $WAVDIR/$L/lists/wav.scp >> $datadir/$L/eval/wav.scp
+    grep -h "$spk" $WAVDIR/$L/lists/spk2utt >> $datadir/$L/eval/spk2utt
+    grep -h "$spk" $WAVDIR/$L/lists/utt2spk >> $datadir/$L/eval/utt2spk
+    grep -h "$spk" $WAVDIR/$L/lists/utt2len >> $datadir/$L/eval/utt2len
+  done
+  ) &
+  wait;
+  sed -e 's?[0-9]*$??' $datadir/${L}/eval/utt2spk \
+  > $datadir/${L}/eval/utt2lang
+
+  ./local/utt2lang_to_lang2utt.pl $datadir/${L}/eval/utt2lang \
+  > $datadir/${L}/eval/lang2utt
+done
+wait;
+echo "Done"
+for L in $TEST_LANGUAGES; do
+  (
+  mkdir -p $tmpdir/test/$L
+  grep "^$L" $test_list | cut -f2- | tr ' ' '\n' \
+    | sed -e "s?^?$L?" -e 's?$?_?' > $tmpdir/test/$L/test_spk
+
+  echo "Language - ${L}: formatting test data."
+  mkdir -p $datadir/$L/test
+  rm -f $datadir/$L/test/wav.scp $datadir/$L/test/spk2utt \
+        $datadir/$L/test/utt2spk $datadir/$L/test/utt2len
+
+  for spk in `cat $tmpdir/test/$L/test_spk`; do
+    grep -h "$spk" $WAVDIR/$L/lists/wav.scp >> $datadir/$L/test/wav.scp
+    grep -h "$spk" $WAVDIR/$L/lists/spk2utt >> $datadir/$L/test/spk2utt
+    grep -h "$spk" $WAVDIR/$L/lists/utt2spk >> $datadir/$L/test/utt2spk
+    grep -h "$spk" $WAVDIR/$L/lists/utt2len >> $datadir/$L/test/utt2len
+  done
+  ) &
+  wait;
+  sed -e 's?[0-9]*$??' $datadir/${L}/test/utt2spk \
+  > $datadir/${L}/test/utt2lang
+
+  ./local/utt2lang_to_lang2utt.pl $datadir/${L}/test/utt2lang \
+  > $datadir/${L}/test/lang2utt
+done
+wait;
+
+echo "Finished data preparation."
---
+++ gp-xvectors-recipe/prep_all_data.sh
@@ -0,0 +1,234 @@
+#!/bin/bash -u
+
+# Copyright 2012  Arnab Ghoshal
+# Copyright 2016 by Idiap Research Institute, http://www.idiap.ch
+# Copyright 2018/2019 by Sam Sucik and Paul Moore
+# See the file COPYING for the licence associated with this software.
+# Author(s):
+#   Bogdan Vlasenko, February 2016
+#   Sam Sucik, Paul Moore, 2018/2019
+
+[ -f helper_functions.sh ] && source ./helper_functions.sh \
+  || echo "helper_functions.sh not found. Won't be able to set environment variables and similar."
+
+[ -f conf/general_config.sh ] && source ./conf/general_config.sh \
+  || echo "conf/general_config.sh not found or contains errors!"
+
+[ -f conf/user_specific_config.sh ] && source ./conf/user_specific_config.sh \
+  || echo "conf/user_specific_config.sh not found, create it by cloning " + \
+          "conf/user_specific_config-example.sh"
+
+[ -f cmd.sh ] && source ./cmd.sh || echo "cmd.sh not found. Jobs may not execute properly."
+
+# CHECKING FOR AND INSTALLING REQUIRED TOOLS:
+#  This recipe requires shorten (3.6.1) and sox (14.3.2).
+#  If they are not found, the local/gp_install.sh script will install them.
+./local/gp_check_tools.sh $home_dir path.sh || exit 1;
+
+. ./path.sh || { echo "Cannot source path.sh"; exit 1; }
+root_data_dir=/home/s1531206/gp-data
+DATADIR=$root_data_dir/all_preprocessed
+rirs_dir=$root_data_dir/RIRS_NOISES
+musan_dir=$root_data_dir/musan
+log_dir=$DATADIR/log
+mfcc_dir=$DATADIR/mfcc
+vaddir=$DATADIR/vad
+
+# Set the languages that will actually be processed
+#GP_LANGUAGES="AR BG CH CR CZ FR GE JA KO PL PO RU SP SW TH TU WU VN"
+GP_LANGUAGES="AR"
+
+echo "Running with languages: ${GP_LANGUAGES}"
+
+# Preparing lists of utterances (and a couple other auxiliary lists) based
+# on the train/enroll/eval/test splitting. The lists refer to the WAVs
+# generated in the previous stage.
+  # NOTE: The wav-dir as it is right now only works in the cluster!
+echo "#### STAGE 1: Organising speakers into sets. ####"
+(
+# Organise data into train, enroll, eval and test
+./local/gp_data_organise_no_combine.sh \
+  --config-dir=$conf_dir \
+  --corpus-dir=$GP_CORPUS \
+  --wav-dir=/mnt/mscteach_home/s1531206/lid/wav \
+  --train-languages="$GP_LANGUAGES" \
+  --enroll-languages="$GP_LANGUAGES" \
+  --eval-languages="$GP_LANGUAGES" \
+  --test-languages="$GP_LANGUAGES" \
+  --data-dir=$DATADIR \
+  || exit 1;
+) > $log_dir/data_organisation
+
+for L in $GP_LANGUAGES; do
+  lang_dir=$home_prefix/${L}
+  train_data=$lang_dir/train
+  enroll_data=$lang_dir/enroll
+  eval_data=$lang_dir/eval
+  test_data=$lang_dir/test
+
+  for data_subset in train enroll eval test; do
+    utils/data/get_utt2num_frames.sh $lang_dir/${data_subset}
+  done
+
+  echo "Splitting data"
+  declare -a times=(3 10 30 60)
+  for data_subset in enroll eval test; do
+    # Make a backup
+    mkdir -p $lang_dir/${data_subset}/.unsplit_backup
+    cp -r $lang_dir/${data_subset}/* $lang_dir/${data_subset}/.unsplit_backup
+    for time in $times; do
+      echo "Splitting ${data_subset} data"
+      ./local/split_long_utts.sh \
+        --max-utt-len $time \
+        $lang_dir/${data_subset} \
+        $lang_dir/${data_subset}/split_${time}s
+    done
+  done
+
+  # Make features and compute the energy-based VAD for each dataset
+  echo "#### Calculating MFCCs and VAD for unsplit data ####"
+  for data_subset in train enroll eval test; do
+    (
+    num_speakers=$(cat $lang_dir/${data_subset}/spk2utt | wc -l)
+    if [ "$num_speakers" -gt "$MAXNUMJOBS" ]; then
+      num_jobs=$MAXNUMJOBS
+    else
+      num_jobs=$num_speakers
+    fi
+
+    echo "Creating 23D MFCC features."
+    steps/make_mfcc.sh \
+      --write-utt2num-frames false \
+      --mfcc-config conf/mfcc.conf \
+      --nj $num_jobs \
+      --cmd "$preprocess_cmd" \
+      --compress true \
+      $lang_dir/${data_subset} \
+      $log_dir/make_mfcc/${data_subset} \
+      $mfcc_dir
+
+    echo "Fixing the directory to make sure everything is fine."
+    utils/fix_data_dir.sh $lang_dir/${data_subset}
+
+    ./local/compute_vad_decision.sh \
+      --nj $num_jobs \
+      --cmd "$preprocess_cmd" \
+      $lang_dir/${data_subset} \
+      $log_dir/make_vad/${data_subset} \
+      $vaddir
+
+    utils/fix_data_dir.sh $lang_dir/${data_subset}
+    ) > $log_dir/${feature_type}_${data_subset}
+  done
+
+  echo "### Calcuating MFCCs and VAD for split data ####"
+  for data_subset in enroll eval test; do
+    (
+    for time in $times; do
+      num_speakers=$(cat $lang_dir/${data_subset}/split_${time}s/spk2utt | wc -l)
+      if [ "$num_speakers" -gt "$MAXNUMJOBS" ]; then
+        num_jobs=$MAXNUMJOBS
+      else
+        num_jobs=$num_speakers
+      fi
+
+      echo "Creating 23D MFCC features."
+      steps/make_mfcc.sh \
+        --write-utt2num-frames false \
+        --mfcc-config conf/mfcc.conf \
+        --nj $num_jobs \
+        --cmd "$preprocess_cmd" \
+        --compress true \
+        $lang_dir/${data_subset}/split_${time}s \
+        $log_dir/make_mfcc/${data_subset}_split_${time}s \
+        $mfcc_dir
+
+      echo "Fixing the directory to make sure everything is fine."
+      utils/fix_data_dir.sh $lang_dir/${data_subset}/split_${time}s
+
+      ./local/compute_vad_decision.sh \
+        --nj $num_jobs \
+        --cmd "$preprocess_cmd" \
+        $lang_dir/${data_subset}/split_${time}s \
+        $log_dir/make_vad/${data_subset}_split_${time}s \
+        $vaddir
+
+      utils/fix_data_dir.sh $lang_dir/${data_subset}/split_${time}s
+      ) > $log_dir/${feature_type}_${data_subset}/split_${time}s
+    done
+  done
+
+  echo "Finished stage 2."
+
+  # Data augmentation step
+  frame_shift=0.01
+  awk -v frame_shift=$frame_shift '{print $1, $2*frame_shift;}' $train_data/utt2num_frames > $train_data/reco2dur
+
+  # Make a version with reverberated speech
+  rvb_opts=()
+  rvb_opts+=(--rir-set-parameters "0.5, $rirs_dir/simulated_rirs/smallroom/rir_list")
+  rvb_opts+=(--rir-set-parameters "0.5, $rirs_dir/simulated_rirs/mediumroom/rir_list")
+
+  # Make a reverberated version of the training data.  Note that we don't add any
+  # additive noise here.
+  # Make sure we have permission to execute (can be weird)
+  chmod +x steps/data/augment_data_dir.py
+  steps/data/reverberate_data_dir.py \
+    "${rvb_opts[@]}" \
+    --speech-rvb-probability 1 \
+    --pointsource-noise-addition-probability 0 \
+    --isotropic-noise-addition-probability 0 \
+    --num-replications 1 \
+    --source-sampling-rate 8000 \
+    ${rirs_dir} \
+    ${train_data} ${train_data}_reverb
+  # Durations are the same
+  cp ${train_data}/utt2dur ${train_data}_reverb
+  cp ${train_data}/vad.scp ${train_data}_reverb
+  utils/copy_data_dir.sh --utt-suffix "-reverb" ${train_data}_reverb ${train_data}_reverb.new
+  rm -rf ${train_data}_reverb
+  mv ${train_data}_reverb.new ${train_data}_reverb
+
+  # Augment with musan_noise
+  steps/data/augment_data_dir.py --utt-suffix "noise" --fg-interval 1 --fg-snrs "15:10:5:0" --fg-noise-dir "${musan_dir}/musan_noise" ${train_data} ${train_data}_noise
+  # Augment with musan_music
+  steps/data/augment_data_dir.py --utt-suffix "music" --bg-snrs "15:10:8:5" --num-bg-noises "1" --bg-noise-dir "${musan_dir}/musan_music" ${train_data} ${train_data}_music
+  # Augment with musan_speech
+  steps/data/augment_data_dir.py --utt-suffix "babble" --bg-snrs "20:17:15:13" --num-bg-noises "3:4:5:6:7" --bg-noise-dir "${musan_dir}/musan_speech" ${train_data} ${train_data}_babble
+
+  # Combine reverb, noise, music, and babble into one directory.
+  utils/combine_data.sh ${train_data}_aug ${train_data}_reverb ${train_data}_noise ${train_data}_music ${train_data}_babble
+
+  # Make MFCCs for the augmented data.  Note that we do not compute a new
+  # vad.scp file here.  Instead, we use the vad.scp from the clean version of
+  # the list.
+  echo "Making MFCCs for augmented data"
+  steps/make_mfcc.sh \
+  --mfcc-config conf/mfcc.conf \
+  --nj $num_jobs \
+  --cmd "$train_cmd" \
+  ${train_data}_aug \
+  $log_dir/make_mfcc \
+  $mfcc_dir
+
+  echo "Tidying up data"
+
+  # Combine the clean and augmented SWBD+SRE list.  This is now roughly
+  # double the size of the original clean list.
+  utils/combine_data.sh ${train_data}_combined ${train_data}_aug ${train_data}
+  utils/fix_data_dir.sh ${train_data}_combined
+
+  # Remove unnecessary folders
+  rm -rf ${train_data}_music
+  rm -rf ${train_data}_noise
+  rm -rf ${train_data}_reverb
+  rm -rf ${train_data}_babble
+  # Have the aug subset and clean data which is enough (both are separate)
+
+  # Get back necessary files for training
+  utils/data/get_utt2num_frames.sh ${train_data}_combined
+  sed -e 's?[0-9]*$??' ${train_data}_combined/utt2spk > ${train_data}_combined/utt2lang
+  local/utt2lang_to_lang2utt.pl ${train_data}_combined/utt2lang > ${train_data}_combined/lang2utt
+
+  echo "Done with data augmentation"
+done
